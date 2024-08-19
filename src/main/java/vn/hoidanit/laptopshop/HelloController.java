221c8b2c7d328854b5e11667919ad306bc039e6e
Test_Terminal_git <minhvnvnvn1998@gmail.com>
2024-08-19 17:06:38 +0700

    secornd comit

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,6 +8,17 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World with MVN!";
+        return "Hello World update with dev-tool 2!";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page !";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page !";
+    }
+
 }
